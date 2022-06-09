6371ddb3321db53da1d2d5a97c610fc2ba8786a5
Frieder Pankratz <frieder.pankratz@gmail.com>
2022-06-09 19:58:43 +0200

    add macros for traact type creation

--- src/traact/spatial.h
+++ src/traact/spatial.h
@@ -1,77 +1,44 @@
 /** Copyright (C) 2022  Frieder Pankratz <frieder.pankratz@gmail.com> **/
 
-#ifndef TRAACT_SPATIAL_MODULE_SRC_TRAACT_SPATIAL_SPATIALPLUGIN_H_
-#define TRAACT_SPATIAL_MODULE_SRC_TRAACT_SPATIAL_SPATIALPLUGIN_H_
+#ifndef TRAACT_SPATIAL_SRC_TRAACT_SPATIAL_H_
+#define TRAACT_SPATIAL_SRC_TRAACT_SPATIAL_H_
 
-#include <traact/buffer/DataFactory.h>
-#include <traact/datatypes.h>
 #include <Eigen/Core>
 #include <Eigen/Geometry>
 #include <Eigen/LU>
+#include <traact/traact.h>
 #include <traact/traact_spatial_export.h>
-#include <rttr/type>
-#include<Eigen/StdVector>
 
 namespace traact::spatial {
 
-typedef typename Eigen::Affine3d Pose6D;
-typedef typename Eigen::Vector2d Position2D;
-typedef typename Eigen::Vector3d Position3D;
-typedef typename std::vector<Eigen::Vector2d, Eigen::aligned_allocator < Eigen::Vector2d> >
-Position2DList;
-typedef typename std::vector<Eigen::Vector3d, Eigen::aligned_allocator < Eigen::Vector3d> >
-Position3DList;
+using Pose6D = Eigen::Transform<traact::Scalar, 3,2>;
+using Rotation3D = Eigen::Quaternion<traact::Scalar >;
+using Translation3D = Eigen::Translation<traact::Scalar, 3>;
+using Position2D = Eigen::Vector<traact::Scalar, 2>;
+using Position3D = Eigen::Vector<traact::Scalar, 3>;
+using Position2DList = std::vector<Eigen::Vector<traact::Scalar, 2>>;
+using Position3DList = std::vector<Eigen::Vector<traact::Scalar, 3>>;
 
-struct TRAACT_SPATIAL_EXPORT Pose6DHeader {
-    /**
-     * Definitions needed by traact and the user to use a datatype
-     */
-    static const char *MetaType;
-    typedef Pose6D NativeType;
-    static const char *NativeTypeName;
-    const size_t size = sizeof(NativeType);
+CREATE_TRAACT_HEADER_TYPE(Pose6DHeader, traact::spatial::Pose6D, "spatial:Pose6D", TRAACT_SPATIAL_EXPORT)
+CREATE_TRAACT_HEADER_TYPE(Rotation3DHeader, traact::spatial::Rotation3D, "spatial:Rotation3D", TRAACT_SPATIAL_EXPORT)
+CREATE_TRAACT_HEADER_TYPE(Translation3DHeader, traact::spatial::Translation3D, "spatial:Translation3D", TRAACT_SPATIAL_EXPORT)
+CREATE_TRAACT_HEADER_TYPE(Position2DHeader, traact::spatial::Position2D, "spatial:Position2D", TRAACT_SPATIAL_EXPORT)
+CREATE_TRAACT_HEADER_TYPE(Position3DHeader, traact::spatial::Position3D, "spatial:Position3D", TRAACT_SPATIAL_EXPORT)
+CREATE_TRAACT_HEADER_TYPE(Position2DListHeader, traact::spatial::Position2DList, "spatial:Position2DList", TRAACT_SPATIAL_EXPORT)
+CREATE_TRAACT_HEADER_TYPE(Position3DListHeader, traact::spatial::Position3DList, "spatial:Position3DList", TRAACT_SPATIAL_EXPORT)
 
- RTTR_ENABLE()
-};
 
-struct TRAACT_SPATIAL_EXPORT Position2DListHeader {
-    /**
-     * Definitions needed by traact and the user to use a datatype
-     */
-    static const char *MetaType;
-    typedef Position2DList NativeType;
-    static const char *NativeTypeName;
-    const size_t size = sizeof(NativeType);
 
- RTTR_ENABLE()
-};
+}// namespace traact::spatial
 
-struct TRAACT_SPATIAL_EXPORT Position3DListHeader {
-    /**
-     * Definitions needed by traact and the user to use a datatype
-     */
-    static const char *MetaType;
-    typedef Position3DList NativeType;
-    static const char *NativeTypeName;
-    const size_t size = sizeof(NativeType);
+#define REGISTER_SPATIAL_COMPONENTS(external_component) \
+REGISTER_COMPONENT(external_component<traact::spatial::Pose6DHeader>) \
+REGISTER_COMPONENT(external_component<traact::spatial::Rotation3DHeader>) \
+REGISTER_COMPONENT(external_component<traact::spatial::Translation3DHeader>) \
+REGISTER_COMPONENT(external_component<traact::spatial::Position2DHeader>) \
+REGISTER_COMPONENT(external_component<traact::spatial::Position3DHeader>)\
+REGISTER_COMPONENT(external_component<traact::spatial::Position2DListHeader>)\
+REGISTER_COMPONENT(external_component<traact::spatial::Position3DListHeader>)
 
- RTTR_ENABLE()
-};
 
-class TRAACT_SPATIAL_EXPORT Pose6DFactoryObject : public buffer::TemplatedDefaultFactoryObject<Pose6DHeader> {
- RTTR_ENABLE(buffer::TemplatedDefaultFactoryObject<Pose6DHeader>, buffer::DataFactory)
-};
-
-class TRAACT_SPATIAL_EXPORT Position2DListFactoryObject : public buffer::TemplatedDefaultFactoryObject<
-    Position2DListHeader> {
- RTTR_ENABLE(buffer::TemplatedDefaultFactoryObject<Position2DListHeader>, buffer::DataFactory)
-};
-
-class TRAACT_SPATIAL_EXPORT Position3DListFactoryObject : public buffer::TemplatedDefaultFactoryObject<
-    Position3DListHeader> {
- RTTR_ENABLE(buffer::TemplatedDefaultFactoryObject<Position3DListHeader>, buffer::DataFactory)
-};
-
-}
-
-#endif //TRAACT_SPATIAL_MODULE_SRC_TRAACT_SPATIAL_SPATIALPLUGIN_H_
+#endif//TRAACT_SPATIAL_SRC_TRAACT_SPATIAL_H_
