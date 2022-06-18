790f2f6ab118b77c2d319eecefbd9a4a207d2b56
Frieder Pankratz <frieder.pankratz@gmail.com>
2022-06-18 10:51:52 +0200

    make Position2D and Position3D OpenCV data types

--- src/traact/spatial.cpp
+++ src/traact/spatial.cpp
@@ -9,11 +9,12 @@ namespace  traact::component::facade {
 
 BEGIN_TRAACT_PLUGIN_REGISTRATION
     REGISTER_DEFAULT_TRAACT_TYPE(traact::spatial::Pose6DHeader)
-    REGISTER_DEFAULT_TRAACT_TYPE(traact::spatial::Position2DListHeader)
-    REGISTER_DEFAULT_TRAACT_TYPE(traact::spatial::Position3DListHeader)
-    REGISTER_DEFAULT_TRAACT_TYPE(traact::spatial::Position3DListHeader)
-    REGISTER_DEFAULT_TRAACT_TYPE(traact::spatial::Position3DListHeader)
-    REGISTER_DEFAULT_TRAACT_TYPE(traact::spatial::Position3DListHeader)
     REGISTER_SPATIAL_COMPONENTS(traact::component::facade::ApplicationAsyncSource)
     REGISTER_SPATIAL_COMPONENTS(traact::component::facade::ApplicationSyncSink)
+//    REGISTER_DEFAULT_TRAACT_TYPE(traact::spatial::Position2DHeader)
+//    REGISTER_DEFAULT_TRAACT_TYPE(traact::vision::Position2DListHeader)
+//    REGISTER_DEFAULT_TRAACT_TYPE(traact::spatial::Position3DHeader)
+//    REGISTER_DEFAULT_TRAACT_TYPE(traact::spatial::Position3DListHeader)
 END_TRAACT_PLUGIN_REGISTRATION
+
+
--- src/traact/spatial.h
+++ src/traact/spatial.h
@@ -14,18 +14,18 @@ namespace traact::spatial {
 using Pose6D = Eigen::Transform<traact::Scalar, 3,2>;
 using Rotation3D = Eigen::Quaternion<traact::Scalar >;
 using Translation3D = Eigen::Translation<traact::Scalar, 3>;
-using Position2D = Eigen::Vector<traact::Scalar, 2>;
-using Position3D = Eigen::Vector<traact::Scalar, 3>;
-using Position2DList = std::vector<Eigen::Vector<traact::Scalar, 2>>;
-using Position3DList = std::vector<Eigen::Vector<traact::Scalar, 3>>;
+//using Position2D = Eigen::Vector<traact::Scalar, 2>;
+//using Position3D = Eigen::Vector<traact::Scalar, 3>;
+//using Position2DList = std::vector<Eigen::Vector<traact::Scalar, 2>>;
+//using Position3DList = std::vector<Eigen::Vector<traact::Scalar, 3>>;
 
 CREATE_TRAACT_HEADER_TYPE(Pose6DHeader, traact::spatial::Pose6D, "spatial:Pose6D", TRAACT_SPATIAL_EXPORT)
 CREATE_TRAACT_HEADER_TYPE(Rotation3DHeader, traact::spatial::Rotation3D, "spatial:Rotation3D", TRAACT_SPATIAL_EXPORT)
 CREATE_TRAACT_HEADER_TYPE(Translation3DHeader, traact::spatial::Translation3D, "spatial:Translation3D", TRAACT_SPATIAL_EXPORT)
-CREATE_TRAACT_HEADER_TYPE(Position2DHeader, traact::spatial::Position2D, "spatial:Position2D", TRAACT_SPATIAL_EXPORT)
-CREATE_TRAACT_HEADER_TYPE(Position3DHeader, traact::spatial::Position3D, "spatial:Position3D", TRAACT_SPATIAL_EXPORT)
-CREATE_TRAACT_HEADER_TYPE(Position2DListHeader, traact::spatial::Position2DList, "spatial:Position2DList", TRAACT_SPATIAL_EXPORT)
-CREATE_TRAACT_HEADER_TYPE(Position3DListHeader, traact::spatial::Position3DList, "spatial:Position3DList", TRAACT_SPATIAL_EXPORT)
+//CREATE_TRAACT_HEADER_TYPE(Position2DHeader, traact::spatial::Position2D, "spatial:Position2D", TRAACT_SPATIAL_EXPORT)
+//CREATE_TRAACT_HEADER_TYPE(Position3DHeader, traact::spatial::Position3D, "spatial:Position3D", TRAACT_SPATIAL_EXPORT)
+//CREATE_TRAACT_HEADER_TYPE(Position2DListHeader, traact::vision::Position2DList, "spatial:Position2DList", TRAACT_SPATIAL_EXPORT)
+//CREATE_TRAACT_HEADER_TYPE(Position3DListHeader, traact::spatial::Position3DList, "spatial:Position3DList", TRAACT_SPATIAL_EXPORT)
 
 
 
@@ -34,20 +34,20 @@ CREATE_TRAACT_HEADER_TYPE(Position3DListHeader, traact::spatial::Position3DList,
 #define CREATE_SPATIAL_COMPONENTS(external_component) \
 CREATE_TEMPLATED_TRAACT_COMPONENT_FACTORY(external_component, traact::spatial, Pose6DHeader) \
 CREATE_TEMPLATED_TRAACT_COMPONENT_FACTORY(external_component, traact::spatial, Rotation3DHeader) \
-CREATE_TEMPLATED_TRAACT_COMPONENT_FACTORY(external_component, traact::spatial, Translation3DHeader) \
-CREATE_TEMPLATED_TRAACT_COMPONENT_FACTORY(external_component, traact::spatial, Position2DHeader) \
-CREATE_TEMPLATED_TRAACT_COMPONENT_FACTORY(external_component, traact::spatial, Position3DHeader)\
-CREATE_TEMPLATED_TRAACT_COMPONENT_FACTORY(external_component, traact::spatial, Position2DListHeader)\
-CREATE_TEMPLATED_TRAACT_COMPONENT_FACTORY(external_component, traact::spatial, Position3DListHeader)
+CREATE_TEMPLATED_TRAACT_COMPONENT_FACTORY(external_component, traact::spatial, Translation3DHeader)
+//CREATE_TEMPLATED_TRAACT_COMPONENT_FACTORY(external_component, traact::spatial, Position2DHeader) \
+//CREATE_TEMPLATED_TRAACT_COMPONENT_FACTORY(external_component, traact::spatial, Position3DHeader)\
+//CREATE_TEMPLATED_TRAACT_COMPONENT_FACTORY(external_component, traact::spatial, Position2DListHeader)\
+//CREATE_TEMPLATED_TRAACT_COMPONENT_FACTORY(external_component, traact::spatial, Position3DListHeader)
 
 #define REGISTER_SPATIAL_COMPONENTS(external_component) \
 REGISTER_TEMPLATED_DEFAULT_COMPONENT(external_component, Pose6DHeader) \
 REGISTER_TEMPLATED_DEFAULT_COMPONENT(external_component, Rotation3DHeader) \
-REGISTER_TEMPLATED_DEFAULT_COMPONENT(external_component, Translation3DHeader) \
-REGISTER_TEMPLATED_DEFAULT_COMPONENT(external_component, Position2DHeader) \
-REGISTER_TEMPLATED_DEFAULT_COMPONENT(external_component, Position3DHeader)\
-REGISTER_TEMPLATED_DEFAULT_COMPONENT(external_component, Position2DListHeader)\
-REGISTER_TEMPLATED_DEFAULT_COMPONENT(external_component, Position3DListHeader)
+REGISTER_TEMPLATED_DEFAULT_COMPONENT(external_component, Translation3DHeader)
+//REGISTER_TEMPLATED_DEFAULT_COMPONENT(external_component, Position2DHeader) \
+//REGISTER_TEMPLATED_DEFAULT_COMPONENT(external_component, Position3DHeader)\
+//REGISTER_TEMPLATED_DEFAULT_COMPONENT(external_component, Position2DListHeader)\
+//REGISTER_TEMPLATED_DEFAULT_COMPONENT(external_component, Position3DListHeader)
 
 
 #endif//TRAACT_SPATIAL_SRC_TRAACT_SPATIAL_H_
